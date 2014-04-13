521f42c5dee4e565cdb863c20aa22152326798e5
ljleppan <leppanen.leo@gmail.com>
2014-04-13 10:03:11 +0300

    JSONUtil now parses JSON to News objects

--- src/com/futurice/festapp/util/JSONUtil.java
+++ src/com/futurice/festapp/util/JSONUtil.java
@@ -1,9 +1,21 @@
 package com.futurice.festapp.util;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+
+import com.futurice.festapp.domain.News;
+import com.futurice.festapp.domain.NewsArticle;
+
 public class JSONUtil {
+	
+	private static final String TAG = "JSONParser";
 	
 	public static String getString(JSONObject j, String name) throws JSONException {
 		if (!j.has(name) || j.isNull(name)) {
@@ -11,11 +23,49 @@ public class JSONUtil {
 		}
 		return j.getString(name);
 	}
+	
 	public static Long getLong(JSONObject j, String name) throws JSONException {
 		if(!j.has(name) || j.isNull(name)) {
 			return null;
 		}
 		return j.getLong(name);
+	}
+	
+	public static List<News> parseNewsFromJSON(String json) throws JSONException {
+		List<News> articles = new ArrayList<News>();
+		JSONArray list = new JSONArray(json);
+		
+		for (int i=0; i < list.length(); i++) {
+			try {
+				JSONObject newsObject = list.getJSONObject(i);
+				News article = parseSingleNewsArticleFromJSON(newsObject);
+				articles.add(article);
+			} catch (Exception e) {
+				Log.w(TAG, "Received invalid JSON-structure", e);
+			}
+		}
+		
+		return articles;
+	}
+	
+	private static News parseSingleNewsArticleFromJSON(JSONObject newsObject) throws JSONException{
+		try {
+			String title = JSONUtil.getString(newsObject, "title");
+			String teaserText = JSONUtil.getString(newsObject, "teaser_text");
+			String content = JSONUtil.getString(newsObject, "content");
+			Date time = CalendarUtil.parseDateFromString(JSONUtil.getString(newsObject, "time"));
+			
+			News article = new News();
+			article.setTitle(title);
+			article.setTeaserText(teaserText);
+			article.setContent(content);
+			article.setTime(time);
+			
+			return article;
+		} catch (Exception e) {
+			Log.w(TAG, "Received invalid JSON-structure", e);
+			return null;
+		}
 	}
 
 }
